02225941ae2ca88ff1ce338530c655fca0772739
safa2015 <safaeuchi2015@gmail.com>
2021-06-24 11:27:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import safa from "./safa.jpg"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="jihed">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/-LfSE9ZxZDI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+      <div className="img" style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+
+
+        <br />
+
+        <img src="/jiji.jpg" alt="jiji" />
+
+        <br />
+
+        <img src={safa} alt ='safaa' />
+
+      
+      </div>
+
     </div>
   );
 }
